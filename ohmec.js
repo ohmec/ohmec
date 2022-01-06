9f347a0ffa10a9eda635a7377a0327eb7fd7fad7
Scott Johnson <sjcupertino@gmail.com>
2022-01-06 11:23:19 -0800

    Move all of geo_features.js routines into ohmec.js
    Signed-off-by: Scott Johnson <sjcupertino@gmail.com>

--- ohmec.js
+++ ohmec.js
@@ -58,8 +58,6 @@ let NLPopupText = '<div id="nlpopup"><p>Data of the indigenous peoples is provid
   '<p>The dates have been added by OHMEC temporarily as from approximately 700 - 1768AD. ' +
   'OHMEC will work with Native Land Digital to attempt to get more accurate representation of where each tribe was and when.</p>';
 
-let styleMatches = {};
-
 for(let param of parameters) {
   let test = /(startdatestr|enddatestr|curdatestr)=([\d:BC-]+)/;
   let match = param.match(test);
@@ -512,7 +510,7 @@ function getTextLabel(bounds, id, label, isPoint, properties, fontinfo, altPrope
     inner += '<text text-anchor="' + anchor + '"';
     inner += ' font-family="' + fontinfo.name + ', Courier, sans-serif"';
     inner += ' fill="' + fontinfo.color + '"';
-    inner += ' font-size="' + fontsize.toFixed(2) + 'px"';
+    inner += ' font-size="' + thisFontsize.toFixed(2) + 'px"';
     if("labelRotate" in properties || "labelX" in properties || "labelY" in properties) {
       inner += ' transform="';
       if("labelRotate" in properties) {
@@ -535,6 +533,50 @@ function getTextLabel(bounds, id, label, isPoint, properties, fontinfo, altPrope
   }
   textLabel.innerHTML = inner;
   return textLabel;
+}
+
+function getFeatureLabel(feature) {
+  if("entity2name" in feature.properties) {
+    return feature.properties.entity2name;
+  } else if(feature.properties.entity1type === 'geography' && feature.properties.entity1name === 'icecap') {
+    return ' '
+  } else {
+    return feature.properties.entity1name;
+  }
+}
+
+function getFeatureFont(feature) {
+  // default styles
+  let fontname   = 'sans serif';
+  let fontscale  = 80;
+  let fontcolor  = "black";
+
+  if("style" in feature) {
+    // scale the font based upon the family, since some are wider than others
+    switch(feature.style.fontname) {
+      case 'Rubik':                fontscale = 81; break;
+      case 'Cabin Sketch':         fontscale = 87; break;
+      case 'Corben':               fontscale = 77; break;
+      case 'New Tegomin':          fontscale = 84; break;
+      case 'Special Elite':        fontscale = 81; break;
+      case 'Fredericka the Great': fontscale = 81; break;
+      case 'Rye':                  fontscale = 73; break;
+      case 'Akaya Telivigala':     fontscale = 94; break;
+      case 'MedievalSharp':        fontscale = 85; break;
+      case 'Benne':                fontscale = 91; break;
+    }
+    return {
+      name:  feature.style.fontname,
+      scale: fontscale,
+      color: feature.style.fontcolor
+    };
+  } else {
+    return {
+      name:  fontname,
+      scale: fontscale,
+      color: fontcolor
+    };
+  }
 }
 
 function onEachFeature(feature, layer) {
@@ -941,6 +983,45 @@ for (let doi=0;doi<idsPerDOI.length;doi++) {
         im++;
       }
     }
+  }
+}
+
+function featureStyle(feature) {
+  // default styles
+  let strokeOn      = true;       // hides feature boundaries if false
+  let strokeOpacity = 1.0;
+  let strokeColor   = 'white';
+  let strokeWeight  = 2.0;
+  let strokeDash    = '3';
+  let fillOn        = true;
+  let fillColor     = '#c0c0c0';
+  let fidelity      = feature.properties.fidelity;
+  let fillOpacity   = 0.2 + fidelity/10;
+
+  // returning all style contents even if default, just to have as reference
+  // (see https://leafletjs.com/reference-1.7.1.html#path-option)
+  if("style" in feature) {
+    return {
+      stroke:       feature.style.strokeOn,
+      opacity:      feature.style.strokeOpacity,
+      color:        feature.style.strokeColor,
+      weight:       feature.style.strokeWeight,
+      dashArray:    feature.style.strokeDash,
+      fill:         feature.style.fillOn,
+      fillOpacity:  feature.style.fillOpacity,
+      fillColor:    feature.style.fillColor
+    };
+  } else {
+    return {
+      stroke:       strokeOn,
+      opacity:      strokeOpacity,
+      color:        strokeColor,
+      weight:       strokeWeight,
+      dashArray:    strokeDash,
+      fill:         fillOn,
+      fillOpacity:  fillOpacity,
+      fillColor:    fillColor
+    };
   }
 }
 
